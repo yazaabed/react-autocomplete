6eac79248d45d3bfced554f29be1c59aa559da5a
Yazan Aabed <yazanaabed2@gmail.com>
2018-10-05 00:26:23 +0300

    Add build file and fix wrong relative path on build distribution folder

---
+++ build.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+let buildPath = process.env.BUILD_PATH;
+
+let fileContent = `
+'use strict';
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod/index.js');
+} else {
+  module.exports = require('./dev/index.js');
+}
+`;
+
+console.log("Start appending index.js...");
+
+fs.writeFile(`./${buildPath}/index.js`, fileContent, function(err) {
+  if(err) {
+    return console.log(err);
+  }
+
+  console.log("Append index.js done...");
+});
+
+/**
+ * Change main entry for json after build
+ */
+let packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+
+packageJson['main'] = 'index.js';
+
+console.log("........................................");
+console.log("Start appending package.json...");
+
+fs.writeFile(`./${buildPath}/package.json`, JSON.stringify(packageJson, null, "  "), function(err) {
+  if(err) {
+    return console.log(err);
+  }
+
+  console.log("Append package.json done...");
+});
